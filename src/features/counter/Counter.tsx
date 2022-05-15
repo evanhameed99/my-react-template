6eb44ee6cf0e47f32a5ecc34a60ce1310c0008be
Evan Hameed <63654749+evanhameed99@users.noreply.github.com>
2022-05-15 20:54:39 +0300

    init app

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useAppSelector, useAppDispatch } from '../../state-manager/hooks';
 import {
   decrement,
   increment,
